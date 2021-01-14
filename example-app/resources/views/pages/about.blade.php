bcfa215507132c496123239756a5fc5bc517c404
David <advitameternam@gmail.com>
2021-01-14 10:56:03 +0100

    Add proper page titles

--- example-app/resources/views/pages/about.blade.php
+++ example-app/resources/views/pages/about.blade.php
@@ -1,6 +1,8 @@
 @extends('base')
+@section('title', 'About Us | Hello City')
 
 @section('content')
+
     <p>Built with &hearts; by David PEREZ.</p>
 
     <p><a href='/'>Revenir à la page d'accueil</a></p>
