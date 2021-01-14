2df9477fd853f86a5388840bcce763bb98157416
David <advitameternam@gmail.com>
2021-01-14 10:31:22 +0100

    Remove duplication by using base template

--- example-app/resources/views/pages/about.blade.php
+++ example-app/resources/views/pages/about.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>About us | Hello City</title>
-</head>
+@section('content')
     <p>Built with &hearts; by David PEREZ.</p>
 
-    <a href='/'>Revenir à la page d'accueil</a>
-
-    
-    <footer>
-        <p>&copy; Copyright {{date('Y')}}</p>
-    </footer>
-</body>
-
-</html>
+    <p><a href='/'>Revenir à la page d'accueil</a></p>
+@endsection
--- example-app/resources/views/pages/home.blade.php
+++ example-app/resources/views/pages/home.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Hello City</title>
-</head>
-
-<body>
+@section('content')
     <h1>Bonjour de Montcuq !</h1>
 
     <p>Il est exactement {{date('H:i')}}.</p>
-    <footer>
-        <p>&copy; Copyright {{date('Y')}} &middot; <a href='/about-us'>À propos</a></p>
-    </footer>
-</body>
-
-</html>
+@endsection
