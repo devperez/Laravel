4221c939a242604241f1ebdd4f4b4e56060d87fe
David <advitameternam@gmail.com>
2021-01-14 15:22:44 +0100

    make use of asset function helper to load images

--- example-app/resources/views/pages/about.blade.php
+++ example-app/resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 
 
 @section('content')
-    <img src="/img/avatar David PRZ.PNG" height="150px" alt="avatar de David PRZ">
+    <img src="{{asset("img/avatar David PRZ.PNG")}}" height="150px" alt="avatar de David PRZ">
     <p>Built with &hearts; by David PEREZ.</p>
 
     <p><a href="{{route('home')}}">Revenir à la page d'accueil</a></p>
--- example-app/resources/views/pages/home.blade.php
+++ example-app/resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('content')
-<img src="/img/Blason_ville_fr_Montcuq_46.svg.png" height="150px" alt="Blason du village de Montcuq">
+<img src="{{asset("img/Blason_ville_fr_Montcuq_46.svg.png")}}" height="150px" alt="Blason du village de Montcuq">
     <h1>Bonjour de Montcuq !</h1>
 
     <p>Il est exactement {{date('H:i')}}.</p>
